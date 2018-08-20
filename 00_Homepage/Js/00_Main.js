1d7df4fc0141451e8817f9428f7eed10faf5148a
JeanLepeix <jean@jeanlepeix.com>
2018-08-20 12:39:37 +0200

    avancement homepage
    reste
    js div regles et texte regles
    position titre
    font titre et boutons
    animations boutons

---
+++ 00_Homepage/Js/00_Main.js
@@ -0,0 +1,7 @@
+let rules = document.querySelector("#rules");
+let titreRules = document.querySelector("#titre-regles");
+let pRules = document.querySelector("#paragraph-regles");
+
+function toggle(item, State) {
+    item.style.display = State;
+}
