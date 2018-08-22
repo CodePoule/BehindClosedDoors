bf7ba106c407bfddf5b58d79ef17e63039e47797
SheraKassi <41896968+SheraKassi@users.noreply.github.com>
2018-08-22 16:10:29 +0200

    Add files via upload

--- Template/Template_Js/Template_Main.js
+++ Template/Template_Js/Template_Main.js
@@ -9,3 +9,52 @@ function closeNav() {
     document.getElementById("main").style.marginLeft= "0";
     document.body.style.backgroundColor = "white";
 }
+
+// FONCTION VIE
+function togglevisi (item,State){
+    item.style.display = State;
+}
+
+let heart1 = document.querySelector("#heart1");
+let heart2 = document.querySelector("#heart2");
+let heart3 = document.querySelector("#heart3");
+
+togglevisi(heart1, "");
+togglevisi(heart2, "");
+togglevisi(heart3, "");
+
+let vie = 3;
+
+function decrementation_vie() {
+    if (vie == 2) {
+        togglevisi(heart1, "none");
+    }
+    if (vie == 1) {
+        togglevisi(heart1, "none");
+        togglevisi(heart2, "none");
+    }
+    if (vie == 0) {
+        togglevisi(heart1, "none");
+        togglevisi(heart2, "none");
+        togglevisi(heart3, "none");
+    }
+}
+
+// FONCTION FLECHE
+let fleche = document.querySelector(".fleche");
+togglevisi(fleche, "none");
+
+
+// !!!! A INSERER LORS DE LA PERTE
+// decrementation de la vie
+vie--;
+decrementation_vie();
+// retour salle 1 si vie=0
+if (vie==0) {
+    setTimeout (function () {document.location.href = "http://localhost:63342/BehindClosedDoors-celinehubertcoutant-patch-1/01_ChildrensMemories/01_Index.html?_ijt=b3mpmg87e33011009urs6s9g97"
+    }, 3000);
+}
+// !!!!! A INSERER LORS DE LA GAGNE
+
+// apparition de la fleche salle suivante
+togglevisi(fleche, "");
