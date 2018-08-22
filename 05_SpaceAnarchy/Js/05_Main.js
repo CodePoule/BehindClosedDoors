21cc02c7e1bff25bac0cee0f4a5a19bbdaa09b8a
SheraKassi <41896968+SheraKassi@users.noreply.github.com>
2018-08-22 11:21:37 +0200

    Add files via upload
    22/08/2018 modif code simon

--- 05_SpaceAnarchy/Js/05_Main.js
+++ 05_SpaceAnarchy/Js/05_Main.js
@@ -1,9 +1,6 @@
 // DEFINITION DES let varIABLES
 
-// let variable pour déterminer si le jeu est éteint ou allumé
-let power = "off";
-// let variable pour le choix du mode: si erreur => retour au debut du jeu
-let strict = "off";
+
 // let variable pour checker si le bouton start a été cliqué ou non
 let running = false;
 // tableau de mémoire de l'ordinateur
@@ -38,89 +35,10 @@ let red = new colorButton(2, "red");
 let yel = new colorButton(3, "yel");
 let blu = new colorButton(4, "blu");
 
-// Lorsque l'on clique sur le switch on/off
-$("#powerSwitch").on("click", function () {
-    // au depart power == "off"
-    if (power == "off") { // on allume le jeu
-        // on rend visible le bouton "On" (carré bleu positionné à gauche)
-        $("#buttonOn").css("visibility", "visible");
-        // on rend invisible le bouton "Off" (carré bleu positionné à droite)
-        $("#buttonOff").css("visibility", "hidden");
-        // on augmente l'opacité du texte (compteur)
-        $("#displayText").css("opacity", "1");
-        // la let variable power vaiut 'on'
-        power = "on";
-    }
-    // si le jeu est sur on, alors au clic:
-    else if (power == "on") { // on éteint le jeu
-        // on rend invisible le bouton on
-        $("#buttonOn").css("visibility", "hidden");
-        // et on rend visible le bouton off
-        $("#buttonOff").css("visibility", "visible");
-        // on diminue l'opacité du compteur
-        $("#displayText").css("opacity", "0.3");
-        // on reinitialise les let variables power et strict sur "off"
-        power = "off";
-        strict = "off";
-        // le bouton strict redevient jaune (il est vert s'il est coché)
-        $("#strictButton").css("background", "yellow");
-        // on retire le "check" du bouton strict
-        $("#strictButton").removeClass("fa fa-check");
-        // la let variable 'running' est remise sur "false" (elle est true quand on a cliqué sur le bouton start)
-        running = false;
-        // le bouton start est remis en rouge (il est vert lorsqu'il est activé au clic)
-        $("#startButton").css("background", "red");
-        // le tableau de mémoire est vidé
-        memoryArray = [];
-        // le tableau des entrées de l'utilisateur est vidé
-        userArray = [];
-        // le niveau est réinitialisé à 1
-        levelCount = 1;
-        // affichage '--' dans le compteur
-        $("#displayText").html("--");
-        // le compteur du tableau de mémoire est remis à 0
-        memoryArrayCounter = 0;
-        // le compteur du tableau des entrées utilisateur est remis à 0
-        userArrayCounter = 0;
-        // la let variable permettant de comparer les 2 tableaux vaut 'true'
-        matchingArrays = true;
-        // efface le timer lié à runMemory (setTimeout)
-        clearInterval(runMemory);
-        // on ne permet plus d'avoir d'evenement sur la souris => on ne peut plus cliquer sur les couleurs (il ne se passe rien)
-        $(".fourButton").css("pointer-events", "none");
-    }
-});
 
-//Visual changes when clicking the strict button
-$("#strictButton").on("click", function () {
-    // on peut cliquer sur le bouton strict uniquement si le jeu est allumé (power=='on') et que l'on a pas encore cliqué sur start (running==false)
-    if (power == "on" && running == false) {
-
-        // au clic, si la case n'avait pas été cliquée
-        if (strict == "off") {
-            // on change la couleur du background qui passe de jaune à vert
-            $("#strictButton").css("background", "green");
-            // la case est cochée
-            $("#strictButton").addClass("fa fa-check");
-            // la let variable strict vaut "on"
-            strict = "on";
-        }
-        // sinon
-        else if (strict == "on") {
-            // on change la couleur du background qui passe de vert à jaune
-            $("#strictButton").css("background", "yellow");
-            // la case est décochée
-            $("#strictButton").removeClass("fa fa-check");
-            // la let variable strict vaut "off"
-            strict = "off";
-        }
-    }
-});
 
 //Effects when clicking the start button
 $("#startButton").on("click", function () {
-    // si le jeu est allumé
-    if (power == "on") {
         // on change le background qui passe de jaune à vert
         $("#startButton").css("background", "green");
         // la let variable running vaut true
@@ -132,6 +50,8 @@ $("#startButton").on("click", function () {
         // on remet les compteur des tableaux à 0
         memoryArrayCounter = 0;
         userArrayCounter = 0;
+        // on augmente l'opacité du texte (compteur)
+        $("#displayText").css("opacity", "1");
         // on réinitialise le niveau à 1
         levelCount = 1;
         // on affiche "--" dans le compteur
@@ -142,7 +62,7 @@ $("#startButton").on("click", function () {
         clearInterval(runMemory);
         // on désactive la possibilté de cliquer sur les boutons
         $(".fourButton").css("pointer-events", "none");
-        // on appel newMemory pour ajouter une couleur au tableau de memoire de l'ordinateur
+        // on appelle newMemory pour ajouter une couleur au tableau de memoire de l'ordinateur
         newMemory();
 
         console.log(memoryArray);
@@ -153,7 +73,7 @@ $("#startButton").on("click", function () {
         setTimeout(function () {
             runMemory = setInterval(playMemory, 1000);
         }, 1000);
-    }
+
 });
 
 //Effects when clicking colored buttons
@@ -161,7 +81,7 @@ $("#startButton").on("click", function () {
 // [id*='button'] selectionne tous les elements id qui contiennent "button"
 $("div[id*='button']").on("click", function () {
     // si le jeu est allumé (power) et que start a été cliqué (running)
-    if (power == "on" && running) {
+    if (running) {
         // event.which == 1 <=> clic de la souris
         if (event.which == 1) {
             // on recucupe le son lié au bouton sur lequel on vient de cliquer dans le html (this.id<=> buttonColor)
@@ -200,36 +120,29 @@ $("div[id*='button']").on("click", function () {
                 matchingArrays = true;
                 // on enleve la possibilité de cliquer sur les boutons
                 $(".fourButton").css("pointer-events", "none");
-
-                // SI LE MODE STRICT EST ACTIVE
-                if (strict == "on") {
-                    // on vide le tableau de memoire de l'ordinateur
-                    memoryArray = [];
-                    // on remet le niveau à 1
-                    levelCount = 1;
-                    // on rappel la fonction permettant de remplir à nouveau le tableau de l'ordinateur
-                    newMemory();
-                    console.log(memoryArray);
-                    // on lance le setTimeout
-                    setTimeout(function () {
-                        runMemory = setInterval(playMemory, tempo);
-                    }, 1000);
-                }
-                // SI LE MODE STRICT EST DESACTIVE
-                // on relance simplement le setTimeout
-                else {
-                    setTimeout(function () {
-                        runMemory = setInterval(playMemory, tempo);
-                    }, 1000);
-                }
+                // on vide le tableau de memoire de l'ordinateur
+                memoryArray = [];
+                // on remet le niveau à 1
+                levelCount = 1;
+                // on rappelle la fonction permettant de remplir à nouveau le tableau de l'ordinateur
+                newMemory();
+                console.log(memoryArray);
+                // tous les boutons s'allument
+                lightLoose();
+                // on lance le setTimeout
+                //  setTimeout(function () {
+                //      runMemory = setInterval(playMemory, tempo);
+                //     }, 1000);
             }
             else {
                 // si les compteurs des tableaux sont égaux
                 if (userArrayCounter == memoryArrayCounter) {
+                    console.log("Comparaison des deux tableaux");
                     // et que matchingArrays vaut true
                     if (matchingArrays) {
-                        // et que le niveau =20
-                        if (levelCount == 20) {
+                        console.log("Les tableaux sont les mêmes");
+                        // et que le niveau =3
+                        if (levelCount == 3) {
                             // on appel la fonction win
                             win();
                         }
@@ -240,30 +153,38 @@ $("div[id*='button']").on("click", function () {
                             // on remet les compteur a 0
                             memoryArrayCounter = 0;
                             userArrayCounter = 0;
-                            // on rappel newMemory pour remplir à nouveau le tableau de l'ordinateur
-                            newMemory();
                             // on augmente le niveau
                             levelCount++;
+                            console.log("Passage au niveau suivant");
+                            console.log("Niveau n° " + levelCount);
+                            // on rappelle newMemory pour remplir à nouveau le tableau de l'ordinateur
+                            newMemory();
 
                             // en fonction du niveau de difficulté, on diminue le temps lié a la fonctipon setTimeout qui permet d'afficher les couleurs de moins en moins lgtps'
                             switch (levelCount) {
                                 case 1:
-                                case 2:
-                                case 3:
-                                case 4:
                                     tempo = 1000;
                                     break;
-                                case 5:
-                                    tempo = 700;
-                                    break;
-                                case 9:
+                                case 2:
                                     tempo = 500;
                                     break;
-                                case 13:
+                                case 3:
                                     tempo = 300;
                                     break;
+                                // case 4:
+                                //     tempo = 1000;
+                                //     break;
+                                // case 5:
+                                //     tempo = 700;
+                                //     break;
+                                // case 9:
+                                //     tempo = 500;
+                                //     break;
+                                // case 13:
+                                //     tempo = 300;
+                                //     break;
                             }
-                            // on rappel la fonction setTimeout qui permet de visualiser les boutons sur lesquels on doit cliquer
+                            // on rappelle la fonction setTimeout qui permet de visualiser les boutons sur lesquels on doit cliquer
                             setTimeout(function () {
                                 runMemory = setInterval(playMemory, tempo);
                             }, 1000);
@@ -280,33 +201,61 @@ $("div[id*='button']").on("click", function () {
 //Randomly selects a number for each level count and
 //Adds it to the memoryArray.  Numbers correspond to
 //colors depending on the color's object.id
+
 function newMemory() {
-    // renvoie le plus grand nombre entier d'un nombre aléatoire*4 +1 => le resultat de temp peut let ier entre 1 et 4
-    let temp = Math.floor((Math.random() * 4) + 1);
-    // en fonction du resulat de temp
-    switch (temp) {
-        // si temp == 1
-        case 1:
-            // on entre "Gre" dans le tableau de mémoire de l'ordinateur
+    function buttonColor(){
+        let temp = Math.floor((Math.random() * 4) + 1);
+
+        if (temp == 1) {
             memoryArray.push("Gre");
-            break;
-        // si temp == 2
-        case 2:
-            // on entre "Red" dans le tableau de mémoire de l'ordinateur
+            console.log("green");
+        }
+        if (temp == 2) {
             memoryArray.push("Red");
-            break;
-        // si temp == 3
-        case 3:
-            // on entre "Yel" dans le tableau de mémoire de l'ordinateur
+            console.log("red");
+        }
+        if (temp == 3) {
             memoryArray.push("Yel");
-            break;
-        // si temp == 4
-        case 4:
-            // on entre "Blue" dans le tableau de mémoire de l'ordinateur
+            console.log("yellow");
+        }
+        if (temp == 4) {
             memoryArray.push("Blu");
-            break;
+            console.log("blue");
+        }
+    }
+
+    // variable compteur pour compter le nombre de combi a ajouter a chq niveau
+    let i;
+
+    if (levelCount == 1) {
+        for (i = 1; i <= 3; i++) { // ajoute 3 valeurs aleatoires dans le tableau
+            console.log("niveau 1");
+            // console.log("temp est égale à :" + temp);
+            buttonColor();
+        }
+        console.log(memoryArray);
+    }
+
+    if (levelCount == 2) {
+        for (i = 1; i <= 2; i++) { // ajoute 2 valeurs dns le tableau
+            console.log("niveau 2");
+            // console.log("temp est égale à :" + temp);
+            buttonColor();
+        }
+        console.log(memoryArray);
+    }
+
+    if (levelCount == 3) { // ajoute 2 valeurs dns le tableau
+        for (i = 1; i <= 2; i++) {
+            console.log("niveau 3");
+            // console.log("temp est égale à :" + temp);
+            buttonColor();
+        }
+        console.log(memoryArray);
     }
 }
+
+
 
 // Fonction playMemory liée au setTimeout (runMemory)
 // cette fonction permet en gros de changer la couleur et les sons des boutons liés au tableau de mémoire de k'ordinateur
@@ -343,6 +292,16 @@ function playMemory() {
 function win() {
     // on affiche WIN dans le compteur
     $("#displayText").html("WIN");
+    light()
+}
+
+function lightLoose () {
+    $("#displayText").html("--");
+    $("#startButton").css("background", "red");
+    light();
+}
+
+function light(){
     // on passe le bouton green dans la classe activated pour qu'il s'eclaire
     $("#buttonGre").addClass("activated");
     // on remove puis ajoute chaque bouton dans la classe "activated" pour qu'ils clignotent l'un après l'autre
@@ -419,29 +378,4 @@ function win() {
     setTimeout(function () {
         $("#buttonBlu").removeClass("activated");
     }, 2000);
-
-    // on rappel la fonction setTimeout
-    setTimeout(function () {
-        // reinitialisation des tableaux et des compteurs
-        userArray = [];
-        memoryArray = [];
-        memoryArrayCounter = 0;
-        userArrayCounter = 0;
-        // retour au niveau 1
-        levelCount = 1;
-        // afficchage de "--" dans le compteur
-        $("#displayText").html("--");
-        // reinitialisation de matchingArray
-        matchingArrays = true;
-        // reinitialisation du timer
-        clearInterval(runMemory);
-        // desactivation des clics possibles
-        $(".fourButton").css("pointer-events", "none");
-        // appel de la fonction newMemory et setTimeout pour démarrer un nouveau jeu
-        newMemory();
-        console.log(memoryArray);
-        setTimeout(function () {
-            runMemory = setInterval(playMemory, 1000);
-        }, 1000);
-    }, 3000);
 }
